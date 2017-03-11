5cf6c1ccf084ff02a76ff74e231176c4aefa6b4b
dh4rm4 <dh4rm4@protonmail.com>
2017-03-11 13:50:33 +0100

    add .zshrc + add emacs config

---
+++ emacs/.emacs.d/elpa/ctable-20140304.1659/ctable-autoloads.el
@@ -0,0 +1,15 @@
+;;; ctable-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ctable.el") (22723 57719 224390 58000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ctable-autoloads.el ends here
