b003000c5ec3a6444ba2b6b9e8f0aced3d54707f
朗喜宝 <dh4rm4@users.noreply.github.com>
2019-03-22 15:23:26 +0100

    Update get_oh_my_zsh.sh

--- basics/Oh-my-zsh/get_oh_my_zsh.sh
+++ basics/Oh-my-zsh/get_oh_my_zsh.sh
@@ -1,5 +1,5 @@
-#sudo apt-get install zsh
-#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo apt-get install zsh -yy
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's:env zsh -l::g' | sed 's:chsh -s .*$::g')"
 cp odin.zsh-theme ~/.oh-my-zsh/themes/
 cp zshrc ~/.zshrc
 source ~/.zshrc
