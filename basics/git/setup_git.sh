26cdcaeea91fad0ca840117b656b04e5284e98e2
dh4rm4 <dh4rm4@protonmail.com>
2019-03-27 23:45:24 +0100

    update git config

--- basics/git/setup_git.sh
+++ basics/git/setup_git.sh
@@ -3,3 +3,4 @@ git config --global user.name "dh4rm4"
 git config --global user.email "dh4rm4@protonmail.com"
 git config --global gpg.program gpg2
 git config --global commit.gpgsign true
+git config --global user.signingkey 01FECC451684BDE1
