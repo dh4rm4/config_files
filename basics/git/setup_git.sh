c78d8d9821d1e0fda3e826498413b6b346563ec7
dh4rm4 <dh4rm4@protonmail.com>
2019-03-27 17:47:49 +0100

    update git setup

--- basics/git/setup_git.sh
+++ basics/git/setup_git.sh
@@ -1,5 +1,5 @@
 echo "[+] Setup git"
-
-
+git config --global user.name "dh4rm4"
+git config --global user.email "dh4rm4@protonmail.com"
 git config --global gpg.program gpg2
 git config --global commit.gpgsign true
